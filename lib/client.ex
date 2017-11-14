db6088f7d386528aeb92526f2739de0cdac73ebe
sourabh2k15 <sourab.reddy2k14@gmail.com>
2017-11-14 18:24:58 -0500

    fixed tabs

--- lib/client.ex
+++ lib/client.ex
@@ -1,25 +1,27 @@
 defmodule Client do
     def contact_server(ip) do
-		name = String.to_atom(Util.randstr() <> "@127.0.0.1")
-		_ = System.cmd("epmd", ['-daemon'])
+	name = String.to_atom(Util.randstr() <> "@127.0.0.1")
+	_ = System.cmd("epmd", ['-daemon'])
 
-		{:ok, _} = Node.start name
-		b = Node.connect(String.to_atom("head_node@"<>ip))
+	{:ok, _} = Node.start name
+	b = Node.connect(String.to_atom("head_node@"<>ip))
 
-		if b do
-			:global.sync()
-			:global.registered_names()
-            Client.request_work
-            receive do
+	if b do
+	    :global.sync()
+	    :global.registered_names()
+            
+	    Client.request_work
+            
+	    receive do
                 {msg} -> msg 
             end
-		else	
-			IO.puts "failed to connect to head_node"
-		end
+	else	
+            IO.puts "failed to connect to head_node"
 	end
+    end
 
     def request_work do
         pid = :global.whereis_name("head")
-		send(pid, {:worker_request, Node.self()})
+	send(pid, {:worker_request, Node.self()})
     end
 end
